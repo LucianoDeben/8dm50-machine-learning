8b15053a345ac69c0b5b00af0b8a592d804acfe2
LucidCodeAI <lutchocode@gmail.com>
2024-09-04 12:12:33 +0200

    Add inital KNN regression solution

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.stats import mode
 
-def knn(X_train, y_train, X_test, k):
+def knn(X_train, y_train, X_test, k, type="classification"):
     predictions = []
     
     # Compute the distances between each test point and all training points
@@ -9,8 +9,14 @@ def knn(X_train, y_train, X_test, k):
         distances = np.linalg.norm(X_train - x_test, axis=1)
         k_neighbors_indices = np.argsort(distances)[:k]
         k_neighbors_labels = y_train[k_neighbors_indices]
-        most_common_label = mode(k_neighbors_labels).mode[0]
-        predictions.append(most_common_label)
+        
+        if type == "classification":
+            most_common_label = mode(k_neighbors_labels).mode[0]
+            predictions.append(most_common_label)
+        elif type == "regression":
+            average_target = np.mean(k_neighbors_labels)
+            predictions.append(average_target)
+        else:
+            raise ValueError("Invalid type. Expected 'classification' or 'regression'.")
     
     return np.array(predictions)
-
