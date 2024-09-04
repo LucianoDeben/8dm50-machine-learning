9da1c7401647960a1e7bbcb436ca1fa7e852d266
LucidCodeAI <lutchocode@gmail.com>
2024-09-04 12:01:28 +0200

    Refactor knn classification solution

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -1,17 +1,16 @@
 import numpy as np
-from collections import Counter
+from scipy.stats import mode
 
-def knn(X, y, x, k):
-    # Compute the distance between x and all examples in the training set
-    distances = np.sqrt(np.sum((X - x) ** 2, axis=1))
-
-    # Sort by distance and return indices of the first k neighbors
-    k_indices = np.argsort(distances)[:k]
-
-    # Extract the labels of the k nearest neighbor training samples
-    k_nearest_labels = y[k_indices]
-
-    # Return the most common class label
-    most_common = Counter(k_nearest_labels).most_common(1)
-    return most_common[0][0]
+def knn(X_train, y_train, X_test, k):
+    predictions = []
     
+    # Compute the distances between each test point and all training points
+    for x_test in X_test:
+        distances = np.linalg.norm(X_train - x_test, axis=1)
+        k_neighbors_indices = np.argsort(distances)[:k]
+        k_neighbors_labels = y_train[k_neighbors_indices]
+        most_common_label = mode(k_neighbors_labels).mode[0]
+        predictions.append(most_common_label)
+    
+    return np.array(predictions)
+
