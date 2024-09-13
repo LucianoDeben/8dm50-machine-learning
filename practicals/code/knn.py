aa98766b86b7311a5bc590cb514c1cdd2c687b74
LucidCodeAI <lutchocode@gmail.com>
2024-09-13 09:14:49 +0200

    Add refactorization week 1

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -2,6 +2,31 @@ import numpy as np
 from scipy.stats import mode
 
 def knn(X_train, y_train, X_test, k, type="classification"):
+    """k-Nearest Neighbors algorithm.
+    
+    Parameters
+    ----------
+    X_train : array_like
+        The training data.
+    y_train : array_like
+        The target values.
+    X_test : array_like
+        The test data.
+    k : int
+        The number of neighbors to consider.
+    type : str
+        The type of problem. Either 'classification' or 'regression'.
+        
+    Returns
+    -------
+    array_like
+        The predictions for the test data.
+        
+    Raises
+    ------
+    ValueError
+        If type is not 'classification' or 'regression'.    
+    """   
     predictions = []
     
     # Compute the distances between each test point and all training points
